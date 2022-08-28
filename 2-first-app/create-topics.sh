a3d03c3b1587e453d03dac0c5d9b1c281cae470a
Loic DIVAD <loicm.divad@gmail.com>
2022-08-28 17:43:00 +0200

    Setup handson-2 (video)

--- 2-first-app/create-topics.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# create the input topic with one partitions
-kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic fruit-input
-
-# create the output topic
-kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic fruit-output-count
